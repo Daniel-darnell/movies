f141f2aac32d500d47389c4be8dc9db71164d54b
Daniel-darnell <darnelldanny1997@gmail.com>
2020-10-21 12:01:49 +0300

    Created a search function

--- app/views.py
+++ app/views.py
@@ -1,6 +1,6 @@
 from flask import render_template
 from app import app
-from .request import get_movies, get_movie
+from .request import get_movies, get_movie, search_movie
 
 #views
 
@@ -39,3 +39,14 @@ def index():
     now_showing_movie = get_movies('now_playing')
     title = 'Home - Welcome to The best Movie Review Website Online'
     return render_template('index.html', title = title, popular = popular_movies, upcoming = upcoming_movie, now_showing = now_showing_movie )
+
+@app.route('/search/<movie_name>')
+def search(movie_name):
+    '''
+    View function to display the search results
+    '''
+    movie_name_list = movie_name.split(" ")
+    movie_name_format = "+".join(movie_name_list)
+    searched_movies = search_movie(movie_name_format)
+    title = f'search results for {movie_name}'
+    return render_template('search.html',movies = searched_movies)
