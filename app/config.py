92240a9690d689398a6067409cf9d2997d7d4b05
Daniel-darnell <darnelldanny1997@gmail.com>
2020-10-21 11:11:13 +0300

    Added more movie categories

--- app/config.py
+++ app/config.py
@@ -3,7 +3,7 @@ class Config:
     General configuration parent class
     '''
     MOVIE_API_BASE_URL ='https://api.themoviedb.org/3/movie/{}?api_key={}'
-
+    MOVIE_API_KEY = '8b3ef2255835b76f2e83d5825cf8b7a1'
 
 class ProdConfig(Config):
     '''
