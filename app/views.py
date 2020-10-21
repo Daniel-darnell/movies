9b7beb3f26dfae270e34dc56af37970c37536a28
Daniel-darnell <darnelldanny1997@gmail.com>
2020-10-21 11:02:09 +0300

    Made the api call

--- app/views.py
+++ app/views.py
@@ -1,16 +1,18 @@
 from flask import render_template
 from app import app
+from .request import get_movies
 
 #views
-@app.route('/')
-def index():
 
-    '''
-    View root page function that returns the index page and its data
-    '''
+##@app.route('/')
+##def index():
 
-    title = 'Home - Welcome to the Best Moview Review Online Website'
-    return render_template('index.html', title = title)
+   ## '''
+    ##View root page function that returns the index page and its data
+    ##'''
+
+    ##title = 'Home - Welcome to the Best Moview Review Online Website'
+    ##return render_template('index.html', title = title)
 
 @app.route('/movie/<int:movie_id>')
 def movie(movie_id):
@@ -19,3 +21,18 @@ def movie(movie_id):
     View movie page function that returns the movie details page and its data
     '''
     return render_template('movie.html',id = movie_id)
+
+
+@app.route('/')
+def index():
+
+    '''
+    View root page function that returns the index page and its data
+    '''
+
+    # Getting popular movie
+    popular_movies = get_movies('popular')
+    print(popular_movies)
+    title = 'Home - Welcome to The best Movie Review Website Online'
+    return render_template('index.html', title = title,popular = popular_movies)
+
